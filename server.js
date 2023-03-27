837063eedb104a37ffae4f4eb31295b7ade94337
Vlad Olekhnovich <u.aliakhnovich@sam-solutions.com>
2023-03-27 12:48:33 +0300

    Just Cause

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('New connection')
-    res.end('Hello there')
+    res.end('Hello there!')
 })
 
 const PORT = process.env.PORT || 8080
