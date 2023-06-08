c71072078bf569d1e23583b394d9dee83dd7b766
Vlad Olekhnovich <u.aliakhnovich@sam-solutions.com>
2023-06-08 17:51:46 +0300

    Updated with code snippet from Google

--- server.py
+++ server.py
@@ -2,6 +2,19 @@ import os
 import socket
 from flask import Flask
 
+from google.auth.transport import requests
+from google.oauth2 import id_token
+
+def validate_iap_jwt(iap_jwt, expected_audience):
+
+    try:
+        decoded_jwt = id_token.verify_token(
+            iap_jwt, requests.Request(), audience='/projects/636510427510/global/backendServices/1021922191998787807',
+            certs_url='https://www.gstatic.com/iap/verify/public_key')
+        return (decoded_jwt['sub'], decoded_jwt['email'], '')
+    except Exception as e:
+        return (None, None, f'**ERROR: JWT validation error {e}**')
+
 app = Flask(__name__)
 
 @app.route("/")
