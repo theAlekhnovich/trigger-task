fb2ad3f26b2eff02b717e7ca522dc87940c9c2b1
Vlad Olekhnovich <u.aliakhnovich@sam-solutions.com>
2023-06-12 17:08:38 +0300

    Try#1411

--- server.py
+++ server.py
@@ -6,17 +6,27 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    key = 'X-Goog-Authenticated-User-Email: accounts.google.com'
+#    key = 'X-Goog-Authenticated-User-Email: accounts.google.com'
+#    value = None
+#    headers = request.headers
+#    if key in headers:
+#        for line in headers[key]:
+#            if line != '':
+#                value = line
+#                break
+#        if value is not None:
+#            return "<h1>Hello there, {value}</h1>"
+#    return "<h1>How the hell you made it here?!</h1>"
+
+    key = 'X-Goog-Authenticated-User-Email'
     value = None
     headers = request.headers
-    if key in headers:
-        for line in headers[key]:
-            if line != '':
-                value = line
-                break
-        if value is not None:
-            return "<h1>Hello there, {value}</h1>"
-    return "<h1>How the hell you made it here?!</h1>"
+    list = [f"{k}: {v}" for k, v in headers]
+    if key in list:
+        for line in key:
+            value = line
+            break
+    return "<h1>Hello there, {value}</h1>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
