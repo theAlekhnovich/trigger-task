977bfadbd3b9c92c189b62a31bfdd6f7eff78236
Vlad Olekhnovich <u.aliakhnovich@sam-solutions.com>
2023-06-12 18:20:16 +0300

    gosh

--- server.py
+++ server.py
@@ -10,20 +10,25 @@ def index():
     key = 'X-Goog-Authenticated-User-Email'
     value = None
     headers = request.headers
-    lines = [f"{k}: {v}" for (k, v) in headers.items()]
-    try:
-        line = next((l for l in lines if key in l), -1)
-        value = line.split(" ")[1].strip("\"\n\r")
-    except StopIteration:
-        pass
-    
-    if value is None:
-        msg = "<h1>Get out of here!</h1>"
-    else:
-        msg = f"<h1>Hello there, {value}!</h1>"
-        
-    return msg
 
+key_in_header = False
+for i, line in enumerate(lines):
+    if key in line:
+        key_in_header = True
+        break
+else:
+    raise ValueError("Header not found.")
+
+if key_in_header:
+    try:
+        line = line.split(' ', 1)[1].strip('\'"')  # Extract the email address between spaces
+        value = line[0]
+    except IndexError:
+        print(f"No email address extracted ({i})")
+        value = ""
+else:
+    print(f"Key '{key}' not found in headers.")
+    value = ""
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
